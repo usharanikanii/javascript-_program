bdf025bda50f208e681579bead86f78ca2af9fc2
KANISHKA S <148430293+usharanikanii@users.noreply.github.com>
2024-07-08 15:06:05 +0530

    Delete 08_JS_ARRAYS directory

--- 08_JS_ARRAYS/ch8_pgm02.js
+++
@@ -1,9 +0,0 @@
-var place1 = { name : "The Pyramids", country : "Egypt" };
-var place2 = { name : "The Grand Canyon", country : "USA" };
-var place3 = { name : "Bondi Beach", country : "Australia" };
-
-var thisYear = [ place1, place2 ];
-var nextYear = [ place3, place1]; //yes there can be two arrays
-
-console.log(thisYear);
-console.log(nextYear);
