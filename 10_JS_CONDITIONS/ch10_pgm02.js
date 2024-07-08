b4410c1e00de589d1e0738f7161b737f5cf70d75
KANISHKA S <148430293+usharanikanii@users.noreply.github.com>
2024-07-08 15:06:19 +0530

    Delete 10_JS_CONDITIONS directory

--- 10_JS_CONDITIONS/ch10_pgm02.js
+++
@@ -1,9 +0,0 @@
-var secret = 8;
-
-var guess = function (userNumber) {
-  if (userNumber === secret) {
-    console.log("Well done!");
-  } else if(userNumber !== secret){
-    console.log("Unlucky, try again.");
-  }
-};
